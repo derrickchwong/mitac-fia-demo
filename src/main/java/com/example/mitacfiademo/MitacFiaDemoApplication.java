426215e96f22ea97ad879d0d1aa0a552d68cecda
Derrick <dwong@pivotal.io>
2020-11-24 10:06:12 +0800

    first commit

---
+++ src/main/java/com/example/mitacfiademo/MitacFiaDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.mitacfiademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MitacFiaDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MitacFiaDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mitacfiademo/MitacFiaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mitacfiademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MitacFiaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
